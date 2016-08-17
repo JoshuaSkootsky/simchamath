0aaa953e2928b0b601027329a7380cf97e3cf6e5
Joshua Skootsky <joshua.skootsky@gmail.com>
2016-08-17 00:21:18 -0400

    Update simchamath.js

--- javascripts/simchamath.js
+++ javascripts/simchamath.js
@@ -3,17 +3,22 @@ function toRadians (angle) {
   return angle * (Math.PI / 180);
 }
 
-var shape_info = document.getElementById('shape_info');
+function getDistance()
+{
+  var shape_info = document.getElementById('shape_info');
 
-var size = shape_info.size;
-var d = shape_info.diameter;
-var difference shape_info.difference;
+  var size = shape_info.size;
+  var d = shape_info.diameter;
+  var difference shape_info.difference;
 
-var theta = 360 * difference / size;
+  var theta = 360 * difference / size;
 
-var theta = toRadians(theta);
+  var theta = toRadians(theta);
 
-var distance = Math.sqrt(
+  var distance = Math.sqrt(
     2 * Math.pow(d/2, 2) - Math.pow(d, 2) * Math.cos(theta) / 2
   );
+  
+  return distance;
+}
 
